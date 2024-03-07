828b54db11d3bd17340300cca35121d9bddaf099
Eyshcharya <eyshcharya@gmail.com>
2024-03-07 12:40:55 +0530

    feat: tab navigation

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import { View, StyleSheet } from 'react-native'
-import CurrentWeather from './src/components/CurrentWeather'
-import UpcomingWeather from './src/components/UpcomingWeather'
+import React from 'react'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import { useGetWeather } from './src/hooks/useGetWeather'
+import ErrorItem from './src/components/ErrorItem'
+import MyStack from './src/components/Nav'
 
 const App = () => {
+  const [loading, error, weather] = useGetWeather()
+
+  if (weather && weather.list && !loading) {
+    return (
+      <NavigationContainer
+        style={{ borderWidth: 2, borderStyle: 'solid', borderColor: 'green' }}
+      >
+        <Tabs weather={weather} />
+      </NavigationContainer>
+    )
+  }
   return (
     <View style={styles.container}>
-      {/* <CurrentWeather /> */}
-      <UpcomingWeather />
+      {error ? (
+        <ErrorItem />
+      ) : (
+        <ActivityIndicator size={'large'} color={'pink'} />
+      )}
     </View>
   )
 }
-
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
     flex: 1
   }
 })
